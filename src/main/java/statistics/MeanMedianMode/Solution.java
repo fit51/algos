9c677027ef0374f61573df75361f281b6444ec4f
p.kondratyuk <p.kondratyuk@tinkoff.ru>
2017-12-11 16:29:51 +0300

    1st Task

--- src/main/java/statistics/MeanMedianMode/Solution.java
+++ src/main/java/statistics/MeanMedianMode/Solution.java
@@ -1,4 +1,6 @@
 package statistics.MeanMedianMode;
+import algo.Algorithm;
+
 import java.io.*;
 import java.util.*;
 
@@ -11,6 +13,42 @@ public class Solution {
         for(int i = 0; i<n; i++) {
             arr[i] = sc.nextInt();
         }
+        Algorithm.QuickSort.quickSort(arr, 0, arr.length - 1);
 
+        long sum = 0;
+        for(int v: arr) {
+//            System.out.print(v + " ");
+           sum += v;
+        }
+
+        //mean
+        double mean = (double)sum/n;
+
+        //median
+        double median = 0;
+        if(n % 2 == 0) {
+            median = (double)(arr[n/2] + arr[n/2 - 1])/2;
+        } else median = arr[n/2];
+
+        //mode
+        int[] count = new int[100001];
+
+        for(int v: arr) {
+            count[v]++;
+        }
+        int maxOccurs = 0;
+        int mode = 0;
+        for(int i=0; i < count.length; i++) {
+            if(count[i] > maxOccurs) {
+                maxOccurs = count[i];
+                mode = i;
+            } else
+                if(count[i] == maxOccurs && mode > i)
+                    mode = i;
+        }
+
+        System.out.println((double) Math.round(mean * 10)/10);
+        System.out.println((double) Math.round(median * 10)/10);
+        System.out.println(mode);
     }
 }
